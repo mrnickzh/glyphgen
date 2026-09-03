3f4576d8c3d796954cbcef3369c303b364c19cb5
mrnickzh <1234dxc@gmail.com>
2026-09-04 02:44:39 +0300

    removed stb_image dependency

--- glyphgen.c
+++ glyphgen.c
@@ -3,11 +3,8 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "lib/stb_truetype.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "lib/stb_image_write.h"
-
-#define ATLAS_WIDTH 512
-#define ATLAS_HEIGHT 512
+#define ATLAS_WIDTH 256
+#define ATLAS_HEIGHT 256
 
 struct {
     int size;
@@ -21,9 +18,9 @@ struct {
 } font_data;
 
 int main(int argc, char **argv) {
-	if (argc < 4) { printf("Usage: glyphgen.exe font.ttf atlas.png glyps.txt\n"); return 0; }
+	if (argc != 4) { printf("Usage: glyphgen.exe font.ttf atlas.txt data.txt\n"); return 0; }
 	
-	font_data.size = 20;
+	font_data.size = 10;
     font_data.atlas_width = ATLAS_WIDTH;
     font_data.atlas_height = ATLAS_HEIGHT;
     font_data.oversample_x = 2;
@@ -56,38 +53,45 @@ int main(int argc, char **argv) {
 		printf("failed to init stb pack\n");
 		return 1;
 	}
-	
 	stbtt_PackSetOversampling(&context, font_data.oversample_x, font_data.oversample_y);
     if (!stbtt_PackFontRange(&context, font_bin, 0, font_data.size, font_data.first_char, font_data.char_count, font_data.char_info)) {
         printf("failed to pack font\n");
 		return 1;
 	}
-	
 	stbtt_PackEnd(&context);
 	
-	if (!stbi_write_png(argv[2], ATLAS_WIDTH, ATLAS_HEIGHT, 1, &atlas_data[0], ATLAS_WIDTH)) {
-		printf("failed to write to png\n");
-		return 1;
-	}
-	printf("png saved!\n");
-	
-	FILE *out_file;
-	out_file = fopen(argv[3], "w");
-	if (out_file == NULL) {
-        printf("error opening out file\n");
+	FILE *atlas_file;
+	atlas_file = fopen(argv[2], "w");
+	if (atlas_file == NULL) {
+        printf("error opening atlas file\n");
         return 1;
     }
+	fprintf(atlas_file, "unsigned char font_atlas[%d] = {\n", ATLAS_WIDTH * ATLAS_HEIGHT);
+	for (int i = 0; i < ATLAS_WIDTH * ATLAS_HEIGHT; i++) {
+		fprintf(atlas_file, "0x%02x,", atlas_data[i]);
+		if (i % 10 == 0) { fprintf(atlas_file, "\n"); }
+	}
+	fprintf(atlas_file, "};\n");
+	printf("atlas saved!\n");
+	fclose(atlas_file);
 	
-	fprintf(out_file, "float font_array[%d] = {\n", font_data.char_count * 10);
+	FILE *data_file;
+	data_file = fopen(argv[3], "w");
+	if (data_file == NULL) {
+        printf("error opening data file\n");
+        return 1;
+    }
+	fprintf(data_file, "float font_data[%d] = {\n", font_data.char_count * 10);
 	for (int chr = 0; chr < font_data.char_count; chr++) {
 		stbtt_aligned_quad quad;
 		float offx = 0.0f, offy = 0.0f;
 		float xadv = font_data.char_info[chr].xadvance;
 		stbtt_GetPackedQuad(font_data.char_info, font_data.atlas_width, font_data.atlas_height, chr, &offx, &offy, &quad, 0);
-		fprintf(out_file, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f,\n", xadv, offy, quad.x0, quad.x1, quad.y0, quad.y1, quad.s0, quad.s1, quad.t0, quad.t1);
+		fprintf(data_file, "%f, %f, %f, %f, %f, %f, %f, %f, %f, %f,\n", xadv, offy, quad.x0, quad.x1, quad.y0, quad.y1, quad.s0, quad.s1, quad.t0, quad.t1);
 	}
-	fprintf(out_file, "}\n");
-	printf("array save!\n");
+	fprintf(data_file, "};\n");
+	printf("data saved!\n");
+	fclose(data_file);
 	
 	return 0;
 }
